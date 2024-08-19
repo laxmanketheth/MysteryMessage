e25093ce4d80e3107fed022eb9ffd4997e68df04
Laxman <laxmanketheth76@gmail.com>
2024-08-19 23:15:36 +0800

    created nextjs project with modal and other basic files

---
+++ src/helpers/sendVerificationEmail.ts
@@ -0,0 +1,25 @@
+import { resend } from "@/lib/resend";
+import VerificationEmail from '../../emails/VerificationEmail';
+import { ApiResponse } from '@/types/ApiResponse';
+
+export async function sendVerificationEmail(
+    email: string,
+    username: string,
+    verifyCode: string
+): Promise<ApiResponse> {
+
+    //setting up Resend to send the opt email to user//
+    try {
+        await resend.emails.send({
+            from: 'onboarding@resend.dev',
+            to: email,
+            subject: 'Mystery message | verification code',
+            react: VerificationEmail({ username, otp: verifyCode }),
+        });
+        return { success: true, message: 'Verification email sent successfully' }
+
+    } catch (emailError) {
+        console.error("Error sending verification email", emailError)
+        return { success: false, message: 'Failed to send verification email' }
+    }
+}
