ce9c34146758d4d2be689a28d40569dcb6e24508
Laxman <laxmanketheth76@gmail.com>
2024-09-21 23:29:17 +0800

    updated typerror in /u/[username]

--- src/app/(auth)/verify/[username]/page.tsx
+++ src/app/(auth)/verify/[username]/page.tsx
@@ -21,7 +21,7 @@ const VerifyAccount = () => {
         resolver: zodResolver(verifySchema),
     })
 
-    const onSubmit = async (data: z.infer<typeof verifySchema>) => {
+    const onSubmit = async (data:any) => {
         try {
             const response = await axios.post(`/api/verify-code`, {
                 username: params.username,
