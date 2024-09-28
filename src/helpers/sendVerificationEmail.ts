ae916096e04023de4a9a0d5c61089949ff5273e7
Laxman <laxmanketheth76@gmail.com>
2024-09-29 00:04:18 +0800

    replaced Resend with Brevo to send verification code to user email when registering

--- src/helpers/sendVerificationEmail.ts
+++ src/helpers/sendVerificationEmail.ts
@@ -1,28 +1,43 @@
-import { resend } from "@/lib/resend";
-import VerificationEmail from '../../emails/VerificationEmail';
+
+/// email verification implementation ////
+
 import { ApiResponse } from '@/types/ApiResponse';
+
+const brevo = require('@getbrevo/brevo');
+let apiInstance = new brevo.TransactionalEmailsApi();
+
+let apiKey = apiInstance.authentications['apiKey'];
+apiKey.apiKey = process.env.BREVO_API_KEY
 
 export async function sendVerificationEmail(
     email: string,
     username: string,
     verifyCode: string
 ): Promise<ApiResponse> {
-
-    //setting up Resend to send the opt email to user//
     try {
-        // console.log('this is email inside sendVerificationEmail file line 13',email);
-        
-      const response =  await resend.emails.send({
-            from: 'mysterymsg@resend.dev',
-            to: email,
-            subject: 'Mystery message | verification code',
-            react: VerificationEmail({ username, otp: verifyCode }),
+        let sendSmtpEmail =  new brevo.SendSmtpEmail();
+
+        sendSmtpEmail.subject = "Mystery Msg Verification Code";
+        sendSmtpEmail.htmlContent = `<html><body><h2>Hello ${username}</h2><h4>Thank you for registering with Mystery Msg. Please use the following verification code to complete your registration: </h4><h1>${verifyCode}</h1></body></html>`;
+        sendSmtpEmail.sender = { "name": "Mystery Msg", "email": "laxmanketheth76@gmail.com" };
+        sendSmtpEmail.to = [
+            { "email" : email , "name": "Mystery Msg"}
+        ];
+        sendSmtpEmail.replyTo = { "email": "laxmanketheth76@gmail.com", "name": "Mystery Msg" };
+        sendSmtpEmail.headers = { "Some-Custom-Name": "unique-id-1234" };
+        // sendSmtpEmail.params = { "" };
+
+        await apiInstance.sendTransacEmail(sendSmtpEmail).then(function (data:any) {
+            console.log('API called successfully. Returned data: ' + JSON.stringify(data));
+        }, function (error:any) {
+            console.error(error);
         });
-        console.log('this is response inside sendVerificationEmail file line 21',response);
+
         return { success: true, message: 'Verification email sent successfully' }
+
 
     } catch (emailError) {
         console.error("Error sending verification email", emailError)
         return { success: false, message: 'Failed to send verification email' }
     }
-}
+};
